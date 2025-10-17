78b08909c81e06e2856248220d2833c05b541d1d
xXChicoGamerXX <148535887+kilianomar5@users.noreply.github.com>
2025-10-17 09:41:55 +0100

    Update functions.php

--- noticias_kilian/functions.php
+++ noticias_kilian/functions.php
@@ -1,13 +1,11 @@
 <?php
 function noticias_kilian_setup() {
-    // Soporte para menús
     register_nav_menus(array(
         'menu_principal' => __('Menú Principal', 'noticias_kilian'),
         'menu_legal'     => __('Menú Legal', 'noticias_kilian'),
         'menu_social'    => __('Menú Social', 'noticias_kilian')
     ));
 
-    // Soporte para imágenes destacadas
     add_theme_support('post-thumbnails');
 }
 add_action('after_setup_theme', 'noticias_kilian_setup');
